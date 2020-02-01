db27e87929f1e84f46763f3e20c0a3402bb14253
Kat <kmneil2@gmail.com>
2020-01-31 20:37:36 -0500

    Step 1

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Add validation!');
+// console.log('Add validation!');
+
+// let formInput = document.querySelector('form')
+// let blankError = document.querySelector('.input-field')
+
+// formInput.addEventListener('submit', function(){
+// if(formInput.checkValidity.valueMissing){
+// blankError = ('Please enter this information!')
+// }
+// })
+
+let parkingForm = document.querySelector("#parking-form");
+let boxes = document.querySelectorAll(".box");
+console.log(boxes);
+let errorMessages = document.querySelector('input-field')
+
+parkingForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    for (let box of boxes) {
+        if (box.value !== "") {
+            box.parentElement.classList.add("input-valid")
+            console.log(box, "valid")
+        }
+        else {
+            box.parentElement.classList.add("input-invalid")
+            console.log(box, "invalid")
+              
+            
+
+        }    
+    }
+
+    })
