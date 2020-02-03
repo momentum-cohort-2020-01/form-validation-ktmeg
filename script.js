6c83e02a783d358c64a8e204ea5a80d4048c2d26
Kat <kmneil2@gmail.com>
2020-02-03 13:38:57 -0500

    Step 2 complete

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ let parkingForm = document.querySelector("#parking-form");
 let boxes = document.querySelectorAll(".box");
 console.log(boxes);
 let errorMessages = document.querySelectorAll(".input-field")
+let name = document.querySelector("#name")
 
 parkingForm.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -23,24 +24,27 @@ parkingForm.addEventListener("submit", function (e) {
         } else {
             box.parentElement.classList.add("input-invalid");
             console.log(box, "invalid");
-
-            // for (let message of errorMessages) {
-            // for (let message = 0; i < message.length; message++) {
+            const newDiv = document.createElement("div");
+            let ermsg = document.createTextNode("This field is required!");
+            newDiv.appendChild(ermsg);
+            box.parentElement.appendChild(newDiv);
            
             // this works, but adds with every submit//
-            // box.insertAdjacentHTML("afterend", " is required"); 
+            // box.insertAdjacentHTML("afterend", "Required"); 
         
-            document.body.onload = addElement;
-            function addElement(){
-            let newDiv = document.createElement("div");
-            let message = document.createTextNode("This field is required!");
-            newDiv.appendChild(message);
-            document.body.insertAfter(newDiv, errorMessages);
+            // document.body.onload = addElement;
+            // function addElement(){
+            // const newDiv = document.createElement("div");
+            // let ermsg = document.textContent("This field is required!");
+            // newDiv.parentElement.createTextNode(ermsg);
+            // document.body.insertAfter(newDiv, errorMessages);
+
+            // console.log("working");
 
         }    
         
         }
-        }
+        })
             //afterbegin does not allow text on form, but shows in console.log//
 
             // for(let msg of errorMessages);
@@ -48,5 +52,4 @@ parkingForm.addEventListener("submit", function (e) {
             //     console.log("is required");
             //     msg.insertAdjacentHTML("afterend", " is required");
 
-            
-            })
+
