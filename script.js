e2747c9555086b1dcd33738dd3c6c3f1795b3e84
Kat <kmneil2@gmail.com>
2020-02-04 10:36:51 -0500

    Changes to Step 2

--- script.js
+++ script.js
@@ -9,47 +9,41 @@
 // }
 // })
 
-let parkingForm = document.querySelector("#parking-form");
-let boxes = document.querySelectorAll(".box");
-console.log(boxes);
-let errorMessages = document.querySelectorAll(".input-field")
-let name = document.querySelector("#name")
+const parkingForm = document.querySelector('#parking-form')
+const boxes = document.querySelectorAll('.box')
+console.log(boxes)
+// const errorMessages = document.querySelectorAll('.input-field')
+// const name = document.querySelector('#name')
 
-parkingForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-    for (let box of boxes) {
-        if (box.value !== "") {
-            box.parentElement.classList.add("input-valid")
-            console.log(box, "valid")
-        } else {
-            box.parentElement.classList.add("input-invalid");
-            console.log(box, "invalid");
-            const newDiv = document.createElement("div");
-            let ermsg = document.createTextNode("This field is required!");
-            newDiv.appendChild(ermsg);
-            box.parentElement.appendChild(newDiv);
-           
-            // this works, but adds with every submit//
-            // box.insertAdjacentHTML("afterend", "Required"); 
-        
-            // document.body.onload = addElement;
-            // function addElement(){
-            // const newDiv = document.createElement("div");
-            // let ermsg = document.textContent("This field is required!");
-            // newDiv.parentElement.createTextNode(ermsg);
-            // document.body.insertAfter(newDiv, errorMessages);
-
-            // console.log("working");
-
-        }    
-        
-        }
-        })
-            //afterbegin does not allow text on form, but shows in console.log//
-
-            // for(let msg of errorMessages);
-            // if (box.value === "") {
-            //     console.log("is required");
-            //     msg.insertAdjacentHTML("afterend", " is required");
+parkingForm.addEventListener('submit', function (e) {
+  e.preventDefault()
+  for (const box of boxes) {
+    if (box.value !== '') {
+      box.parentElement.classList.add('input-valid')
+    } else {
+      box.parentElement.classList.add('input-invalid')
+      const newDiv = document.createElement('div')
+      const ermsg = document.createTextNode('This field is required!')
+      newDiv.appendChild(ermsg)
+      box.parentElement.appendChild(newDiv)
+    //   ermsg.classList.add('text-danger')
+    }
+  }
+//   function clearErrorMessages ()
 
 
+  // this works, but adds with every submit//
+  // box.insertAdjacentHTML("afterend", "Required")
+    // document.body.onload = addElement;
+    // function addElement(){
+    // const newDiv = document.createElement("div");
+    // let ermsg = document.textContent("This field is required!");
+    // newDiv.parentElement.createTextNode(ermsg);
+    // document.body.insertAfter(newDiv, errorMessages);
+    // console.log("working")    
+    // afterbegin does not allow text on form, but shows in console.log//
+    // for(let msg of errorMessages);
+    // if (box.value === "") {
+    //     console.log("is required");
+    //     msg.insertAdjacentHTML("afterend", " is required");
+  })
